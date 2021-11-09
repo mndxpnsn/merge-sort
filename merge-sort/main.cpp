0d941280284d9e4c906de8e5db4b7315f94cfe1c
derek <harrison.derek.w@gmail.com>
2021-11-09 15:48:43 +0100

    remove reference method

--- merge-sort/main.cpp
+++ merge-sort/main.cpp
@@ -11,7 +11,6 @@
 #include <vector>
 
 #include "merge_sort.hpp"
-#include "merge_sort_ref.hpp"
 
 int main(int argc, const char * argv[]) {
 
@@ -26,46 +25,16 @@ int main(int argc, const char * argv[]) {
         int val = rand() % n + 1;
         input.push_back(val);
     }
-    
-    //Perform merge sort using method 1
-    std::vector<int> result = merge_sort_wrap_ref(input);
 
-    //Perform merge sort using method 2
+    //Perform merge sort
     merge_sort_wrap(input);
     
-    //Check if array using method 1 is sorted
-    bool arr_is_sorted = true;
-    int size_res = (int) result.size();
-    for(int i = 0; i < size_res - 1; ++i) {
-        if(result[i] > result[i + 1]) {
-            arr_is_sorted = false;
-        }
-    }
-    
-    //Check if array using method 2 is sorted
-    bool arr_inp_is_sorted = true;
-    int size_res_inp = (int) input.size();
-    for(int i = 0; i < size_res_inp - 1; ++i) {
-        if(input[i] > input[i + 1]) {
-            arr_inp_is_sorted = false;
-        }
-    }
-    
     //Print results
-    int size = (int) result.size();
-    for(int i = 0; i < size; ++i) {
-        std::cout << result[i] << " ";
-    }
-    std::cout << std::endl;
-
     int size_inp = (int) input.size();
     for(int i = 0; i < size_inp; ++i) {
         std::cout << input[i] << " ";
     }
     std::cout << std::endl;
-    
-    std::cout << "array is sorted: " << arr_is_sorted << std::endl;
-    std::cout << "input is sorted: " << arr_inp_is_sorted << std::endl;
     std::cout << "done" << std::endl;
     
     return 0;
